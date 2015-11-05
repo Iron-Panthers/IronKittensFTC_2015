c86546f064926b73bf360c0af40be1b78527a3ec
Wayne Flores <waynef711@gmail.com>
2015-11-04 16:37:53 -0800

    Continued to work on DriveAuto

--- DriveAuto.java
+++ DriveAuto.java
@@ -6,26 +6,48 @@ public class DriveAuto
 	public DcMotor leftMotor;
     public DcMotor rightMotor;
 
+	double RADIUS = 7;
+	double ROBOTWIDTH = 18;
+
 	public void driveStraight(double distance, double speed)
 	{
-		while()
+		double targetValue = distance/(7 * Math.PI);
+		double start = leftMotor.getCurrentPosition();
+
+		while(leftMotor.getCurrentPosition() < targetValue)
 		{
 			leftMotor.setPower(speed);
 			rightMotor.setPower(speed);
 		}
 	}
 
-	public void turn(double angle)
+	public void turnRight(double angle)
 	{
+		double start = leftMotor.getCurrentPosition();
 		leftMotor.setPower(0);
 		rightMotor.setPower(0);
 
-		//turns left
-		leftMotor.setPower(1);
-		rightMotor.setPower(-1);
+		double targetValue = ((18 * Math.PI) * (angle / 360)) / 7 * Math.PI;
 
-		//turns right
-		leftMotor.setPower(-1);
-		rightMotor.setPower(1);
+		while(rightMotor.getCurrentPosition() < targetValue)
+		{
+			leftMotor.setPower(0.5);
+			rightMotor.setPower(-0.5);
+		}
+	}
+
+	public void turnLeft(double angle)
+	{
+		double start = rightMotor.getCurrentPosition();
+		leftMotor.setPower(0);
+		rightMotor.setPower(0);
+
+		double targetValue = ((18 * Math.PI) * (angle / 360)) / 7 * Math.PI;
+
+		while(rightMotor.getCurrentPosition() < targetValue)
+		{
+			rightMotor.setPower(0.5);
+			leftMotor.setPower(-0.5);
+		}
 	}
 }
