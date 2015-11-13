e48cad8c5cc0cc3d52321447b55866522d55525e
Isabel Dominik <icd107@live.com>
2015-11-13 15:57:18 -0800

    maxim
    To whoever took my Microsoft Office: I will find you. You have my Word

--- Climber.java
+++ Climber.java
@@ -1,78 +1,75 @@
-import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.DcMotorController;
-
 public class Climber
 {
-	public DcMotor elevatorMotor;
-	public Servo dumpServo;
+    public DcMotor elevatorMotor;
+    public Servo dumpServo;
 
-		
-	public static final double DUMP_POSITION = 100;
-	public static final double UP_POSITION = 10;
-	public static final double PLATFORM_DOWN = 100;
-	public static final double PLATFORM_UP = 10;
+           
+    public static final double DUMP_POSITION = 100;
+    public static final double UP_POSITION = 10;
+    public static final double PLATFORM_DOWN = 100;
+    public static final double PLATFORM_UP = 10;
 
-	public Climber(HardwareMap hardwareMap)
-	{
-		elevatorMotor = hardwareMap.dcMotor.get("elevatorMotor");
-		dumpServo = hardwareMap.Servo.get("dumpServo");
-	}
-	void setMotorPower(int Motor, double power)
-	{
-		
-	}
-	
-	public void lowerPlatform()
-	{
-   		elevatorMotor.setMotorPower(0.75);
-		if (elevatormotor.getcurrentposition() < PLATFORM_DOWN)
-		{
-			elevatorMotor.setMotorPower(0);
-		}
-    } 
-   	
-   	public void raisePlatform()
-   	{
-		elevatormotor.setMotorPower(0.75)
-		if (elevatormotor.getcurrentposition() > PLATFORM_UP)
-		{
-			elevatorMotor.setMotorPower(0);
-		}
-   	}
-	
-	void setServoPosition(int channel, double position)
-	{
+    public Climber(HardwareMap hardwareMap)
+    {
+        elevatorMotor = hardwareMap.dcMotor.get("elevatorMotor");
+        dumpServo = hardwareMap.Servo.get("dumpServo");
+    }
+    void setMotorPower(int motor, double power)
+    {
+           
+    }
 
-	}  
-    
+    public void lowerPlatform()
+    {
+        elevatorMotor.setMotorPower(0.75);
+        if (elevatormotor.getcurrentposition() < PLATFORM_DOWN)
+        {
+            elevatorMotor.setMotorPower(0);
+        }
+    }
+   
+    public void raisePlatform()
+    {
+        elevatormotor.setMotorPower(0.75)
+        if (elevatormotor.getcurrentposition() > PLATFORM_UP)
+        {
+            elevatorMotor.setMotorPower(0);
+        }
+    }
+   
+    void setServoPosition(int channel, double position)
+    {
+
+    }  
+   
     public void dumpPlatform()
     {
-		dumpServo.setServoPosition(DUMP_POSITION); //value needs to be confirmed
-	}
+        dumpServo.setServoPosition(DUMP_POSITION);
+    }
 
     public void resetPlatform()
-	{
-		dumpServo.setServoPosition(UP_POSITION);
+    { 
+        dumpServo.setServoPosition(UP_POSITION);
     }
-	   
+           
 //elevatormotor.getcurrentposition
-	
-
+       
+ 
 }
-
-
+ 
+ 
                      
-
-//	Motor(elevatorMotor)
+ 
+//      Motor(elevatorMotor)
 //Servo(dumpServo)
 //void dumpPlatform()
-//	servo lower platform
+//      servo lower platform
 //void resetPlatform()
-//	puts platform parallel to floor
+//      puts platform parallel to floor
 //void raisePlatform()
-//	uses both motors
+//      uses both motors
 //void lowerPlatform()
-//	uses both motors[]
+//      uses both motors[]
 // written by BigKobe and twamble
 //classify servo
 //outline steps of the methods
