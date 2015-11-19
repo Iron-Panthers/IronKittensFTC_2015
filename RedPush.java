34d1c0061f8a08ceadd49292cf624fc2cf5b4e11
Drewpew <agcummings11@gmail.com>
2015-11-18 16:54:38 -0800

    Create RedPush.java

---
+++ RedPush.java
@@ -0,0 +1,13 @@
+public class BluePush
+{
+	DriveAuto driveAuto = new DriveAuto();
+	AllianceButton allianceButton = new AllianceButton();
+
+ 	public void go()
+	{
+		driveAuto.driveStraight(107, 0.5);
+		driveAuto.turnLeft(50);
+		allianceButton.execute();
+	}
+
+}
