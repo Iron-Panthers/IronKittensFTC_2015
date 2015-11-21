487ceed7cc04c458b0815a44b195c9ee0ea04659
BattleDoog <austin.soohoo@gmail.com>
2015-11-20 17:55:54 -0800

    Create AutoRedOppRamp.java

---
+++ AutoRedOppRamp.java
@@ -0,0 +1,24 @@
+public class AutoRedOppRamp
+{
+	RedPush redPush = new RedPush();
+	DriveAuto driveAuto = new DriveAuto();
+	AllianceButton allianceButton = new AllianceButton();
+	AutoRedOppRamp(){};
+
+	public void goOppRamp
+	{
+		driveAuto.wait(10);
+		driveAuto.driveStraight(52.1, 0.5);
+		driveAuto.turnRight(90);
+		driveAuto.driveStraight(40, 0.5);
+		while(true)
+		{
+			driveAuto.driveStraight(1, 1);
+			if(allianceButton.checkDist() < 20.0 && allianceButton.checkDist() > 18.0)
+			{
+				break;
+			}
+		}
+	}
+	}
+}
