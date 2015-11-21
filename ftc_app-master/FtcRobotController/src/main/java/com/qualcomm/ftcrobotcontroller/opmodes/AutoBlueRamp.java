00bde42d5c4fbb8f68d2b8bc5b9b015d38e44c2c
BattleDoog <austin.soohoo@gmail.com>
2015-11-21 11:52:29 -0800

    Update AutoBlueRamp.java

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoBlueRamp.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoBlueRamp.java
@@ -1,10 +1,12 @@
 public class AutoBlueRamp
 {
-	DriveAuto driveAuto = new DriveAuto();
-	Mountain mountain = new Mountain();
+	DriveAuto driveAuto;
+	Mountain mountain;
 
 	public void execute()
 	{
+		driveAuto = new DriveAuto();
+		mountain = new Mountain();
 		driveAuto.driveStraight(52.1, 0.5);
 		driveAuto.turnLeft(90);
 		driveAuto.driveStraight(40, 0.5);
