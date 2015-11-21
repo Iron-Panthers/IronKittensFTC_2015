21eb885c851839e04f3cf6e62b67c162326e78c4
BattleDoog <austin.soohoo@gmail.com>
2015-11-20 17:07:08 -0800

    First Draft of case 1
    welcome to auto boys

--- AutoRedRampButt.java
+++ AutoRedRampButt.java
@@ -2,20 +2,25 @@
 
 public class AutoRedRampButt
 {
+	AutoRedRampButt(){};
 	RedPush redPush = new RedPush();
 	DriveAuto driveAuto = new DriveAuto();
 	AllianceButton allianceButton = new AllianceButton();
-
-	public void goFace
+	
+	public void goBR
 	{
 		redPush.go();
 		driveAuto.driveStraight(-31, 0.5);
 		driveAuto.turnLeft(50);
-		while(allianceButton.checkDist() < /*enter value*/)
+		driveAuto.driveStraight(32.68. 0.5);
+		while(true)
 		{
 			driveAuto.driveStraight(1, 1);
+			if(allianceButton.checkDist() < 20.0 && allianceButton.checkDist() > 18.0)
+			{
+				break;
+			}
 		}
-
 		// ROBOT is on mountain now
 	}
 }
